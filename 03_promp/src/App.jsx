985e2c3910a406fb98a12b3a4e5dbfede635d949
prince <princepatel27806@gmail.com>
2026-06-27 16:45:42 +0530

    added

--- 03_promp/src/App.jsx
+++ 03_promp/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <Card user='world' age={100} img='https://images.unsplash.com/photo-1769863467291-23cf7902f686?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDg4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'/>
         <Card user='krish' age={20} img='https://images.unsplash.com/photo-1766503206606-27de0861e8a4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDk5fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'/>
         <Card user='heyyy' age={22}/>
-      
+        <Card user='me' age={20}/>
     </div>
   )
 }
