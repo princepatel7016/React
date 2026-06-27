b8710b6b0454cf6f30865052d5412d7e540b4aa9
prince <princepatel27806@gmail.com>
2026-06-27 10:42:12 +0530

    learning

---
+++ 01_learning/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+<>
+    <div id="parent">
+      <h1 id="child1">hello world</h1>
+      <h2 id="child2">hello</h2>
+          </div>
+          
+            <div>      
+            <h1>hii</h1>
+            </div>
+</>
+  )
+}
+
+export default App
